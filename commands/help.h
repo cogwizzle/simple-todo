69409319ee2748394e40716a1f3092cf70f30b54
Cogwizzle <joseph.fehrman@gmail.com>
2023-04-13 21:23:12 -0400

    feat: Make help easier to read.

--- commands/help.h
+++ commands/help.h
@@ -3,14 +3,18 @@
 using namespace std;
 
 namespace Commands {
+#ifndef HELP_H
+#define HELP_H
   void help() {
     cout << "Todo tool" << endl;
-    cout << "====================" << endl;
-    cout << "Command line arguments:" << endl; cout << " -l   Lists all the tasks" << endl;
+    cout << "=========" << endl;
+    cout << "Command line arguments:" << endl;
+    cout << " -l         Lists all the tasks" << endl;
     cout << " -a {task}  Adds a new task" << endl;
     cout << " -r {index} Removes a task" << endl;
-    cout << " -c {index}  Completes a task" << endl;
-    cout << " -H {date} Lists all the tasks for a given date" << endl;
-    cout << " -h Prints out this help" << endl;
+    cout << " -c {index} Completes a task" << endl;
+    cout << " -H {date}  Lists all the tasks for a given date" << endl;
+    cout << " -h         Prints out this help" << endl;
   }
+#endif
 }
--- commands/list-file-content.h
+++ commands/list-file-content.h
@@ -15,14 +15,14 @@ namespace CommandUtility {
     cout << endl;
     ifstream todoFile;
     try {
-    todoFile.open(fileName);
-    string line;
-    int i = 1;
-    while (getline(todoFile, line)) {
-      cout << i << ". " << line << endl;
-      i++;
-    }
-    todoFile.close();
+      todoFile.open(fileName);
+      string line;
+      int i = 1;
+      while (getline(todoFile, line)) {
+        cout << i << ". " << line << endl;
+        i++;
+      }
+      todoFile.close();
     } catch (const ifstream::failure& e) {
       cout << "No tasks found for that date." << endl;
     }
